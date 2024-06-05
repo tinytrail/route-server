b998e83323e88b51bacb93b757dc131ada3c4eae
Pradeep Sekar <pradeep.sekar@hashicorp.com>
2024-06-04 19:37:23 -0700

    feat: client methods for listFeatures

--- client/client.go
+++ client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"time"
 
 	pb "github.com/tinytrail/route-server/route_guide"
 	"google.golang.org/grpc"
@@ -14,6 +15,14 @@ var (
 	serverAddr = flag.String("addr", "localhost:1111", "The server address in the format of host:port")
 )
 
+func getAndPrintFeature(client *pb.RouteGuideClient, point *pb.Point) {
+	feature, err := (*client).GetFeature(context.Background(), point)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	fmt.Println("Feature: ", feature)
+
+}
 func main() {
 	flag.Parse()
 	var opts []grpc.DialOption
@@ -25,17 +34,37 @@ func main() {
 	}
 	defer conn.Close()
 	client := pb.NewRouteGuideClient(conn)
+	point := pb.Point{Latitude: 409146138, Longitude: -746188906}
+	getAndPrintFeature(&client, &point)
 
-	feature, err := client.GetFeature(context.Background(), &pb.Point{Latitude: 409146138, Longitude: -746188906})
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-	fmt.Println("Feature: ", feature)
+	point = pb.Point{Latitude: 0, Longitude: 0}
+	getAndPrintFeature(&client, &point)
 
-	feature, err = client.GetFeature(context.Background(), &pb.Point{Latitude: 0, Longitude: 0})
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-	fmt.Println("Feature: ", feature)
 
+	rect := pb.Rectangle{Lo: &pb.Point{Latitude: 400000000, Longitude: -750000000}, Hi: &pb.Point{Latitude: 420000000, Longitude: -730000000}}
+	listAndPrintFeatures(&client, &rect)
+	
+}
+
+func listAndPrintFeatures(client *pb.RouteGuideClient, rectangle *pb.Rectangle) {
+		// Provide a context with a timeout
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+	
+		stream, err := (*client).ListFeatures(ctx, rectangle)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+	
+		for {
+			feature, err := stream.Recv()
+			if err != nil {
+				if err.Error() != "EOF" {
+					fmt.Println("Error: ", err)
+				}
+				break
+			}
+			fmt.Printf("Feature: %v with Location: %v, %v\n", feature.GetName(), feature.GetLocation().Latitude, feature.GetLocation().Longitude)
+		}
+	
 }
--- server/server.go
+++ server/server.go
@@ -5,23 +5,27 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"math"
 	"net"
 	"os"
+	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
 
 	pb "github.com/tinytrail/route-server/route_guide"
 )
 
 var (
-	port = flag.Int("port", 1111, "The server port")
+	port     = flag.Int("port", 1111, "The server port")
 	jsonFile = flag.String("json_file", "./server/route_guide_db.json", "A json file containing a list of features")
 )
 
 type routeGuideServer struct {
 	pb.UnimplementedRouteGuideServer
 	savedFeatures []*pb.Feature
-	routeNotes map[string][]*pb.RouteNote
+	routeNotes    map[string][]*pb.RouteNote
 }
 
 func (s *routeGuideServer) loadFeatures(filepath string) {
@@ -30,7 +34,7 @@ func (s *routeGuideServer) loadFeatures(filepath string) {
 	var data []byte
 	var err error
 	data, err = os.ReadFile(filepath)
-	if (err != nil) {
+	if err != nil {
 		fmt.Println("Error: ", err)
 		data = []byte("{}")
 	}
@@ -50,6 +54,96 @@ func (s *routeGuideServer) GetFeature(context context.Context, point *pb.Point) 
 	return &pb.Feature{Location: point, Name: "Unnamed"}, nil
 }
 
+func (s *routeGuideServer) ListFeatures(rect *pb.Rectangle, stream pb.RouteGuide_ListFeaturesServer) error {
+	for _, feature := range s.savedFeatures {
+		if inRange(feature.Location, rect) {
+			stream.Send(feature)
+		}
+	}
+	//stream.Context().Done()
+	return nil
+}
+
+func (s *routeGuideServer) RecordRoute(stream pb.RouteGuide_RecordRouteServer) error {
+	var pointCount, featureCount, distance int32
+	var lastPoint *pb.Point
+	startTime := time.Now()
+	for {
+		point, err := stream.Recv()
+
+		// If the err is EOF, we have reached the end of the stream
+		if err == io.EOF {
+			// End the stream and construct the statistics
+			currentTime := time.Now()
+			return stream.SendAndClose(&pb.RouteSummary{
+				FeatureCount: featureCount,
+				PointCount:   pointCount,
+				Distance:     distance,
+				ElapsedTime:  int32(currentTime.Sub(startTime).Seconds())})
+		}
+
+		// For unknown errors, break the loop and quit
+		if err != nil {
+			fmt.Println("Error: ", err)
+			break
+		}
+
+		pointCount++
+		// Compare the point to an existing feature and add to the count if they match
+		for _, feature := range s.savedFeatures {
+			if proto.Equal(feature.Location, point) {
+				featureCount++
+			}
+		}
+		if lastPoint != nil {
+			distance += int32(math.Sqrt(math.Pow(float64(point.Latitude-lastPoint.Latitude), 2) + math.Pow(float64(point.Longitude-lastPoint.Longitude), 2)))
+		}
+
+		lastPoint = point
+	}
+	return nil
+}
+
+func (s *routeGuideServer) RouteChat(stream pb.RouteGuide_RouteChatServer) error {
+	for {
+		note, err := stream.Recv()
+
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return err
+		}
+
+		// Construct a key for the incoming note
+		key := fmt.Sprintf("%d %d", note.Location.Latitude, note.Location.Longitude)
+
+		// Append the note to the list of notes for the key
+		s.routeNotes[key] = append(s.routeNotes[key], note)
+
+		// Send the note back to the client
+		for _, n := range s.routeNotes[key] {
+			err := stream.Send(n)
+			if err != nil {
+				return err
+			}
+		}
+
+	}
+}
+
+func inRange(point *pb.Point, rect *pb.Rectangle) bool {
+	left := math.Min(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	right := math.Max(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	top := math.Max(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
+	bottom := math.Min(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
+
+	return left <= float64(point.Longitude) && float64(point.Longitude) <= right &&
+		bottom <= float64(point.Latitude) && float64(point.Latitude) <= top
+}
+
 func newServer() *routeGuideServer {
 	s := routeGuideServer{routeNotes: make(map[string][]*pb.RouteNote)}
 	s.loadFeatures(*jsonFile)
@@ -60,7 +154,7 @@ func main() {
 	flag.Parse()
 	fmt.Print("Starting server on port:", *port)
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
-	if (err != nil) {
+	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
@@ -70,6 +164,6 @@ func main() {
 
 	grpcServer := grpc.NewServer(opts...)
 	pb.RegisterRouteGuideServer(grpcServer, newServer())
-	grpcServer.Serve(lis);
+	grpcServer.Serve(lis)
 
 }
