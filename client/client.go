51c0022a88c60c53a6dde9fdea2b0c982c395d49
Pradeep Sekar <pradeep.sekar@hashicorp.com>
2024-06-04 20:14:30 -0700

    feat: client methods for recordRoute, routeChat

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"time"
+	"io"
 
 	pb "github.com/tinytrail/route-server/route_guide"
 	"google.golang.org/grpc"
@@ -22,6 +23,103 @@ func getAndPrintFeature(client *pb.RouteGuideClient, point *pb.Point) {
 	}
 	fmt.Println("Feature: ", feature)
 
+}
+
+func listAndPrintFeatures(client *pb.RouteGuideClient, rectangle *pb.Rectangle) {
+	// Provide a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := (*client).ListFeatures(ctx, rectangle)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	for {
+		feature, err := stream.Recv()
+		if err != nil {
+			if err.Error() != "EOF" {
+				fmt.Println("Error: ", err)
+			}
+			break
+		}
+		fmt.Printf("Feature: %v with Location: %v, %v\n", feature.GetName(), feature.GetLocation().Latitude, feature.GetLocation().Longitude)
+	}
+}
+
+func recordRoute(client *pb.RouteGuideClient) {
+
+	points := []*pb.Point{
+		{Latitude: 409146138, Longitude: -746188906},
+		{Latitude: 0, Longitude: 0},
+		{Latitude: 0, Longitude: 1},
+		{Latitude: 1, Longitude: 0},
+		{Latitude: 1, Longitude: 1},
+	}
+
+	// Create a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := (*client).RecordRoute(ctx)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	for _, point := range points {
+		if err := stream.Send(point); err != nil {
+			fmt.Println("Error: ", err)
+		}
+	}
+
+	summary, err := stream.CloseAndRecv()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	fmt.Println("Route summary: ", summary)
+
+}
+
+func routeChat(client *pb.RouteGuideClient) {
+
+	points := []*pb.RouteNote{
+		{Location: &pb.Point{Latitude: 409146138, Longitude: -746188906}, Message: "First message"},
+		{Location: &pb.Point{Latitude: 0, Longitude: 0}, Message: "Second message"},
+		{Location: &pb.Point{Latitude: 0, Longitude: 1}, Message: "Third message"},
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := (*client).RouteChat(ctx)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	waitc := make(chan struct{})
+	go func() {
+		for {
+			note, err := stream.Recv()
+			if err == io.EOF {
+				close(waitc)
+				return
+			}
+			if err != nil {
+				fmt.Println("Error: ", err)
+				return
+			}
+			fmt.Printf("Got message: %v\n", note)
+		}
+	
+		}()
+
+		for _, point := range points {
+			if err := stream.Send(point); err != nil {
+				fmt.Println("Error: ", err)
+			}
+		}
+		stream.CloseSend()
+		<-waitc
 }
 func main() {
 	flag.Parse()
@@ -44,27 +142,8 @@ func main() {
 	rect := pb.Rectangle{Lo: &pb.Point{Latitude: 400000000, Longitude: -750000000}, Hi: &pb.Point{Latitude: 420000000, Longitude: -730000000}}
 	listAndPrintFeatures(&client, &rect)
 	
+	recordRoute(&client)
+
+	routeChat(&client)
 }
 
-func listAndPrintFeatures(client *pb.RouteGuideClient, rectangle *pb.Rectangle) {
-		// Provide a context with a timeout
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-	
-		stream, err := (*client).ListFeatures(ctx, rectangle)
-		if err != nil {
-			fmt.Println("Error: ", err)
-		}
-	
-		for {
-			feature, err := stream.Recv()
-			if err != nil {
-				if err.Error() != "EOF" {
-					fmt.Println("Error: ", err)
-				}
-				break
-			}
-			fmt.Printf("Feature: %v with Location: %v, %v\n", feature.GetName(), feature.GetLocation().Latitude, feature.GetLocation().Longitude)
-		}
-	
-}
